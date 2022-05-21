aaad1389ce75d1da485e96b2a6702179903d804b
gaon-park <parkgaon@team-lab.com>
2022-05-21 18:27:44 +0900

    프로젝트 생성

--- src/main/java/dol/example/IndexApplication.java
+++ src/main/java/dol/example/IndexApplication.java
@@ -1,6 +1,6 @@
 package dol.example;
 
-public class ExampleApplication {
+public class IndexApplication {
     public static void main(String[] args){
         System.out.println("hello world");
     }
